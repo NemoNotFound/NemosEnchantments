1f602e9268431feb0e063a5462a5b8372408e339
NemoNotFound <02grimes-bard@icloud.com>
2024-11-01 18:05:21 +0100

    Add keeping of soul binding tools when dying

---
+++ src/main/java/com/nemonotfound/nemosenchantments/mixin/PlayerEntityMixin.java
@@ -0,0 +1,53 @@
+package com.nemonotfound.nemosenchantments.mixin;
+
+import com.nemonotfound.nemosenchantments.enchantment.ModEnchantments;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.nemonotfound.nemosenchantments.utils.EnchantmentUtils.hasEnchantment;
+
+@Mixin(PlayerEntity.class)
+public abstract class PlayerEntityMixin extends Entity {
+
+    @Shadow @Final
+    PlayerInventory inventory;
+    @Unique
+    private final Map<Integer, ItemStack> itemStackMap = new HashMap<>();
+
+    public PlayerEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Inject(method = "dropInventory", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;vanishCursedItems()V"))
+    private void dropInventory(CallbackInfo ci) {
+        itemStackMap.clear();
+
+        for (int i = 0; i < this.inventory.size(); i++) {
+            ItemStack itemStack = this.inventory.getStack(i);
+            if (!itemStack.isEmpty() && hasEnchantment(this.getWorld(), ModEnchantments.SOUL_BINDING, itemStack)) {
+                itemStackMap.put(i, itemStack);
+            }
+        }
+    }
+
+    @Inject(method = "dropInventory", at = @At(value = "TAIL"))
+    private void dropInventoryTail(CallbackInfo ci) {
+        for (Map.Entry<Integer, ItemStack> entry : itemStackMap.entrySet()) {
+            this.inventory.setStack(entry.getKey(), entry.getValue());
+        }
+    }
+}
