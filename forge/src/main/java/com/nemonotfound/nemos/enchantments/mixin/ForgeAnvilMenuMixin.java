d3ca0c7da97b698096f349ebc55aa436ff80efff
NemoNotFound <02grimes-bard@icloud.com>
2025-06-01 15:37:29 +0200

    Refactoring

--- forge/src/main/java/com/nemonotfound/nemos/enchantments/mixin/ForgeAnvilMenuMixin.java
+++ forge/src/main/java/com/nemonotfound/nemos/enchantments/mixin/ForgeAnvilMenuMixin.java
@@ -13,16 +13,8 @@ public class ForgeAnvilMenuMixin {
     @Definition(id = "cost", field = "Lnet/minecraft/world/inventory/AnvilMenu;cost:Lnet/minecraft/world/inventory/DataSlot;")
     @Definition(id = "get", method = "Lnet/minecraft/world/inventory/DataSlot;get()I")
     @Expression("this.cost.get() >= 40")
-    @ModifyExpressionValue(method = "createResult", at = @At(value = "MIXINEXTRAS:EXPRESSION", ordinal = 0))
-    private boolean updateFirstResult(boolean original) {
-        return false;
-    }
-
-    @Definition(id = "cost", field = "Lnet/minecraft/world/inventory/AnvilMenu;cost:Lnet/minecraft/world/inventory/DataSlot;")
-    @Definition(id = "get", method = "Lnet/minecraft/world/inventory/DataSlot;get()I")
-    @Expression("this.cost.get() >= 40")
-    @ModifyExpressionValue(method = "createResult", at = @At(value = "MIXINEXTRAS:EXPRESSION", ordinal = 1))
-    private boolean updateSecondResult(boolean original) {
+    @ModifyExpressionValue(method = "createResult", at = @At(value = "MIXINEXTRAS:EXPRESSION"))
+    private boolean updateResult(boolean original) {
         return false;
     }
 }
