3d652765de5e054a3c34521a7e5ed58329443c77
NemoNotFound <02grimes-bard@icloud.com>
2025-07-05 10:11:32 +0200

    Update 1.21.7

--- common/src/main/java/com/nemonotfound/nemos/enchantments/mixin/ServerPlayerEntityMixin.java
+++ common/src/main/java/com/nemonotfound/nemos/enchantments/mixin/ServerPlayerEntityMixin.java
@@ -1,12 +1,12 @@
 package com.nemonotfound.nemos.enchantments.mixin;
 
 import com.mojang.authlib.GameProfile;
-import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,15 +16,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerPlayer.class)
 public abstract class ServerPlayerEntityMixin extends Player {
 
-    @Shadow public abstract ServerLevel serverLevel();
+    @Shadow public abstract @NotNull Level level();
 
-    public ServerPlayerEntityMixin(Level level, BlockPos pos, float yaw, GameProfile gameProfile) {
-        super(level, pos, yaw, gameProfile);
+    public ServerPlayerEntityMixin(Level level, GameProfile gameProfile) {
+        super(level, gameProfile);
     }
 
     @Inject(method = "restoreFrom", at = @At(value = "TAIL"))
     private void restoreFrom(ServerPlayer oldPlayer, boolean alive, CallbackInfo ci) {
-        if (!alive && !(this.serverLevel().getGameRules().getBoolean(GameRules.RULE_KEEPINVENTORY) || oldPlayer.isSpectator())) {
+        var serverLevel = (ServerLevel) this.level();
+
+        if (!alive && !(serverLevel.getGameRules().getBoolean(GameRules.RULE_KEEPINVENTORY) || oldPlayer.isSpectator())) {
             this.getInventory().replaceWith(oldPlayer.getInventory());
         }
     }
--- fabric/src/main/java/com/nemonotfound/nemos/enchantments/datagen/EnchantmentTagProvider.java
+++ fabric/src/main/java/com/nemonotfound/nemos/enchantments/datagen/EnchantmentTagProvider.java
@@ -3,43 +3,45 @@ package com.nemonotfound.nemos.enchantments.datagen;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.EnchantmentTags;
+import net.minecraft.world.item.enchantment.Enchantment;
 
 import java.util.concurrent.CompletableFuture;
 
 import static com.nemonotfound.nemos.enchantments.enchantment.ModEnchantments.*;
 
-public class EnchantmentTagProvider extends FabricTagProvider.EnchantmentTagProvider {
+public class EnchantmentTagProvider extends FabricTagProvider<Enchantment> {
 
-    public EnchantmentTagProvider(FabricDataOutput output, CompletableFuture<HolderLookup.Provider> completableFuture) {
-        super(output, completableFuture);
+    public EnchantmentTagProvider(FabricDataOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
+        super(output, Registries.ENCHANTMENT, registriesFuture);
     }
 
     @Override
     protected void addTags(HolderLookup.Provider provider) {
-        getOrCreateTagBuilder(EnchantmentTags.IN_ENCHANTING_TABLE)
+        builder(EnchantmentTags.IN_ENCHANTING_TABLE)
                 .add(CLIMBER)
                 .add(SPRINTER)
                 .add(FARMERS_KNOWLEDGE)
                 .add(REAPER)
                 .add(REPLANTING);
 
-        getOrCreateTagBuilder(EnchantmentTags.ON_RANDOM_LOOT)
+        builder(EnchantmentTags.ON_RANDOM_LOOT)
                 .add(SOUL_BINDING)
                 .add(MAGMA_WALKER);
 
-        getOrCreateTagBuilder(EnchantmentTags.NON_TREASURE)
+        builder(EnchantmentTags.NON_TREASURE)
                 .add(CLIMBER)
                 .add(SPRINTER)
                 .add(FARMERS_KNOWLEDGE)
                 .add(REAPER)
                 .add(REPLANTING);
 
-        getOrCreateTagBuilder(EnchantmentTags.TREASURE)
+        builder(EnchantmentTags.TREASURE)
                 .add(SOUL_BINDING)
                 .add(MAGMA_WALKER);
 
-        getOrCreateTagBuilder(EnchantmentTags.TRADEABLE)
+        builder(EnchantmentTags.TRADEABLE)
                 .add(SOUL_BINDING)
                 .add(CLIMBER)
                 .add(SPRINTER)
@@ -48,7 +50,7 @@ public class EnchantmentTagProvider extends FabricTagProvider.EnchantmentTagProv
                 .add(REPLANTING)
                 .add(MAGMA_WALKER);
 
-        getOrCreateTagBuilder(EnchantmentTags.BOOTS_EXCLUSIVE)
+        builder(EnchantmentTags.BOOTS_EXCLUSIVE)
                 .add(MAGMA_WALKER);
     }
 }
